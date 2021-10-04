a5c0f2748a0746de467410bb178238168d307564
Stef <stefgijsberts@icloud.com>
2021-10-04 17:04:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="tstl",
+    version="1.0.0",
+    py_modules=["tstl"],
+    entry_points="""
+        [console_scripts]
+        tstl=tstl:main
+    """,
+)
